ce20d8bb02800e6b62bd95a8c67b6651914e920d
Konun32 <aleksey.u.work@mail.ru>
2022-07-11 00:42:56 +0500

    creating and adapting a volume selection block and creating a feedback slider

---
+++ #source/js/script.js
@@ -0,0 +1,35 @@
+"use strict"
+
+//switching buttons
+let buttons = document.querySelectorAll(".volume__button");
+let images = document.querySelectorAll("volume__slide");
+let image = 0;
+if (buttons.length > 0) {
+	for (let index = 0; index < buttons.length; index++) {
+		const button = buttons[index];
+		button.addEventListener("click", function(e) {
+			for (let i = 0; i < buttons.length; i++) {
+				if (buttons[i].classList.contains("volume__button_active")) {
+					buttons[i].classList.remove("volume__button_active");
+				}
+			}
+			if (document.documentElement.clientWidth > 625) {
+				document.querySelector('.volume__slider-line').style.marginLeft = `${-596*(index-image)}px`;
+			} else {
+				document.querySelector('.volume__slider-line').style.marginLeft = `${-93*(index-image)}vw`;
+			}
+			button.classList.add("volume__button_active");
+		})
+	}
+}
+
+//slick slider
+$(document).ready(function(){
+	$('.reviews__slider').slick({
+		arrows: true,
+		dots: true,
+		centerMode: true,
+		//slidesToShow: 1,
+		//variableWidth: true
+	});
+});
